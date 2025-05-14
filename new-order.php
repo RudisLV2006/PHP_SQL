ed5c80eb2b4f75004cb10089f1fbdb47b331314d
Vigo28 <gamingvigo06@gmail.com>
2025-05-14 08:39:02 +0300

    New orders

--- new-order.php
+++ new-order.php
@@ -1,5 +1,14 @@
 <?php
 @require("connection.php");
 
+$sql = "INSERT INTO orders (customer_id, order_date, status)
+VALUES (1011, '2017-01-22', 2)";
 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
